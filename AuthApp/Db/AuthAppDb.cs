5db2789699b3180047cb1a99e5c6d15124a62b8c
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2023-09-01 17:21:03 +0300

    Adding Db

---
+++ AuthApp/Db/AuthAppDb.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuthApp.Db;
+
+public class AuthAppDb : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public AuthAppDb(DbContextOptions<AuthAppDb> options) : base(options){ }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasKey(x => x.Id);
+    }
+}
---
+++ AuthApp/Extensions/DbExtention.cs
@@ -0,0 +1,14 @@
+﻿using AuthApp.Db;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthApp.Extensions;
+
+public static class DbExtention
+{
+    public static IServiceCollection AddAuthDbConfigs(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AuthAppDb>(opts => opts.UseSqlServer(configuration.GetConnectionString("AuthAppDb")));
+
+        return services;
+    }
+}
