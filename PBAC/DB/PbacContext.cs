6fe9582f1653c7106779c1fbc326f5d6a6584480
MaysaM-M-Mousa <maysam.m.mousa@gmail.com>
2024-02-12 21:04:56 +0200

    Adding new project for permission-based access control,  adding ef core and using sql server

---
+++ PBAC/DB/PbacContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PBAC.Models;
+
+namespace PBAC.DB;
+
+public class PbacContext : DbContext
+{
+    public PbacContext(DbContextOptions<PbacContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasKey(x => x.Id);
+    }
+}
---
+++ PBAC/Extensions/DbExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PBAC.DB;
+
+namespace PBAC.Extensions;
+
+public static class DbExtensions
+{
+    public static IServiceCollection AddPbacDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services.AddDbContext<PbacContext>(x => x.UseSqlServer(configuration.GetConnectionString("AuthPbacAppDb")));
+    }
+}
